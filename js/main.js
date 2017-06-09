a96766730c56fd1798d44fa500ce10f39ed0d402
Kyri <me@kyrik.com>
2017-06-09 16:04:58 -0400

    Initial commit
    All project files added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$('.more-info').tooltip({
+	    'placement': 'top',
+	    'html': 'true',
+	    'title': "<div class='slot chosen-subject detail'><p>The course requires a prerequisite</p><p><a href='' title='Add one'>Add one.</a></p></div></div>"
+	});
+});
