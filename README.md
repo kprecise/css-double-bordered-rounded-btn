2e30ca515dcfd836522527c900d89a2b73bfb4b0
kprecise <me@kyrik.com>
2017-06-17 16:45:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Double Bordered Rounded Button
+A rounded button that uses two borders using pure CSS.
+
+Hover state is reversal of colours
--- js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$('.more-info').tooltip({
-	    'placement': 'top',
-	    'html': 'true',
-	    'title': "<div class='slot chosen-subject detail'><p>The course requires a prerequisite</p><p><a href='' title='Add one'>Add one.</a></p></div></div>"
-	});
-});
